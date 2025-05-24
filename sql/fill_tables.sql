721be26a1fdffff776a49131f7b125768eae22f3
Zekeriya Büker <zbuker@gmail.com>
2025-05-24 18:15:06 +0300

    fill_tables.sql deki hata düzeltildi!

--- sql/fill_tables.sql
+++ sql/fill_tables.sql
@@ -1,13 +1,12 @@
 INSERT INTO roles (role_name) 
-VALUES (
+VALUES
     ('teacher'),
-    ('student')
-);
+    ('student');
 
 INSERT INTO users (name, surname, username, email, roleID) 
-VALUES (
+VALUES 
     ('Melis', 'Verici', 'meliver', 'vericimels@hotmail.com', 1), 
-    ('Ali', 'Kemal', 'akemal01@gmail.com', '', 1),
+    ('Ali', 'Kemal', 'ali.kemal', 'akemal01@gmail.com', 1),
     ('Abuzer', 'Kadayıf', 'abukada', 'abuzerkadayif@gmail.com', 2), 
     ('Meltem', 'Sever', 'melosev', 'melosev@gmail.com', 2), 
     ('Şakir', 'Çekiç', 'sakircekic', 'sakir.cekic@hotmail.com', 2), 
@@ -16,5 +15,4 @@ VALUES (
     ('Cevriye', 'Adalı', 'cev.ada', 'cevriye_adali@yandex.com', 2),
     ('Şaban', 'Uzan', 'suzan', 'suzan99@gmail.com', 2),
     ('Meltem', 'Doğru', 'mdogru', 'melo80@gmail.com', 2),
-    ('Ebru', 'Cesur', 'cesurebru', 'ebcesr@yahoo.com', 2)
-);
+    ('Ebru', 'Cesur', 'cesurebru', 'ebcesr@yahoo.com', 2);
