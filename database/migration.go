0b5b9e6081a3ef3076f55aaa9c5e9bdbc137afd2
Zekeriya Büker <zbuker@gmail.com>
2025-06-05 15:30:17 +0300

    CreateDBPool fonksiyonu düzenlendi

--- database/migration.go
+++ database/migration.go
@@ -23,7 +23,10 @@ func InitDBEngine(pool *pgxpool.Pool) {
 	DBEngine = &DBPool{Pool: pool}
 }
 
-func ConnectToDB(url string) (*pgxpool.Pool, error) {
+func CreateDBPool() (*pgxpool.Pool, error) {
+	dbEnv := c.GetDBServerEnv()
+	url := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", dbEnv.Username, dbEnv.Password, dbEnv.Host, dbEnv.Port, dbEnv.DBName)
+
 	config, err := pgxpool.ParseConfig(url)
 	if err != nil {
 		return nil, fmt.Errorf("veritabanı bağlantı URL'si parse edilirken hata oluştu: %v", err)
@@ -31,31 +34,8 @@ func ConnectToDB(url string) (*pgxpool.Pool, error) {
 
 	pool, err := pgxpool.NewWithConfig(context.Background(), config)
 	if err != nil {
-		return nil, err
-	}
-
-	return pool, nil
-}
-
-func OpenDBConnection(dbURL string) (*pgxpool.Pool, error) {
-	pool, err := ConnectToDB(dbURL)
-	if err != nil {
 		return nil, fmt.Errorf("veritabanına bağlanırken hata oluştu: %v", err)
 	}
 
-	return pool, nil
-}
-
-func CreateConnectURL() string {
-	dbEnv := c.GetDBServerEnv()
-
-	return fmt.Sprintf("postgres://%s:%s@%s:%s/%s", dbEnv.Username, dbEnv.Password, dbEnv.Host, dbEnv.Port, dbEnv.DBName)
-}
-
-func CreateDBPool() (*pgxpool.Pool, error) {
-	pool, err := OpenDBConnection(CreateConnectURL())
-	if err != nil {
-		return nil, err
-	}
 	return pool, nil
 }
