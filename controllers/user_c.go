c5d6aadd8a1200dfc231ff7124433df8ab854788
Zekeriya Büker <zbuker@gmail.com>
2025-07-17 12:35:31 +0300

    CreateUser() fonksiyonu oluşturuldu

--- controllers/user_c.go
+++ controllers/user_c.go
@@ -8,11 +8,12 @@ import (
 	m "studentApi/models"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
 
-var users []m.User
 
 func ListUsers(ctx *gin.Context) {
+	var users []m.User
 	rows, err := d.DBEngine.Pool.Query(context.Background(), "SELECT * FROM users")
 	if err != nil {
 		log.Println("DB sorgu hatası: ", err)
@@ -34,5 +35,51 @@ func ListUsers(ctx *gin.Context) {
 }
 
 func CreateUser(ctx *gin.Context) {
+	//var user []m.User
+	user := m.InsertUserRequest{}
 
+	if err := ctx.ShouldBindJSON(&user); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error":"Geçersiz istek! Eksik veya hatalı alan var"})
+		return
+	}
+
+	var roleID uuid.UUID
+	roleIdQuery := `SELECT ID FROM roles WHERE role_name = $1`
+	if err := d.DBEngine.Pool.QueryRow(context.Background(), roleIdQuery, user.RoleName).Scan(&roleID); err != nil {
+		ctx.JSON(http.StatusNotFound, gin.H{"error":"Belirtilen rol bulunamadı"})
+		return
+	}
+
+	insertQuery := `
+		INSERT INTO users (ID, name, surname, username, email, roleID) 
+		VALUES ($1, $2, $3, $4, $5, $6)
+		RETURNING ID
+	`
+	userID := uuid.New()
+	_, err := d.DBEngine.Pool.Exec(
+		context.Background(), 
+		insertQuery, 
+		userID,
+		user.Name,
+		user.Surname, 
+		user.Username,
+		user.Email,
+		roleID,
+	)
+
+	if err != nil {
+		log.Println("Kullanıcı ekleme hatası:", err)
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error":"Kullanıcı eklenemedi"})
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, gin.H{
+		"id" : userID,
+		"name" : user.Name,
+		"surname" : user.Surname, 
+		"username" : user.Username,
+		"email" : user.Email,
+		"role_id" : roleID,
+		"role_name" : user.RoleName,
+	})
 }
--- models/user_m.go
+++ models/user_m.go
@@ -3,10 +3,18 @@ package models
 import "github.com/google/uuid"
 
 type User struct {
-	Id      uuid.UUID `json:"id,omitempty"`
-	Name    string    `json:"name,omitempty"`
-	Surname string    `json:"surname,omitempty"`
+	Id       uuid.UUID `json:"id,omitempty"`
+	Name     string    `json:"name,omitempty"`
+	Surname  string    `json:"surname,omitempty"`
 	Username string    `json:"username,omitempty"`
-	Email  string    `json:"email,omitempty"`
-	RoleId uuid.UUID `json:"role_id,omitempty"`
+	Email    string    `json:"email,omitempty"`
+	RoleId   uuid.UUID `json:"role_id,omitempty"`
+}
+
+type InsertUserRequest struct {
+	Name     string    `json:"name,omitempty"`
+	Surname  string    `json:"surname,omitempty"`
+	Username string    `json:"username,omitempty"`
+	Email    string    `json:"email,omitempty"`
+	RoleName string    `json:"role_name,omitempty"`
 }
